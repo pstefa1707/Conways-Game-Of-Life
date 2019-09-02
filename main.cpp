e4ffbf3ace8f5463be60a457e9f7014022e5ed0a
pstefa1707 <stefap@smc.sa.edu.au>
2019-09-02 19:59:17 +0930

    Added fps counter on screen (SFML draw calls faster than standard out)

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@
 class Grid;
 int main()
 {
+    float window_width = 800;
+    float window_height = 800;
     int grid_width = 200;
     int grid_height = 200;
-    float window_width = 850;
-    float window_height = 850;
+	sf::Font arial;
+	arial.loadFromFile("./lib/arial.ttf");
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Pstefa's Game of Life");
     //window.setFramerateLimit(144);
     Grid grid(grid_width, grid_height, window_width, window_height);
@@ -33,7 +35,6 @@ int main()
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) grid.populate();
         window.clear(sf::Color::White);
-
         for (int x = 0; x < grid.width(); x++)
         {
             for (int y = 0; y < grid.height(); y++)
@@ -49,7 +50,11 @@ int main()
             }
         }
         
-        std::cout << "FrameRate: " << 1.0 / CLOCK.restart().asSeconds() << std::endl;
+	    std::string fps = "FPS: " + std::to_string(1 / CLOCK.restart().asSeconds());
+	    sf::Text FPS(fps, arial, 30);
+        FPS.setFillColor(sf::Color::Black);
+        FPS.setPosition(sf::Vector2f(window_width - 200, window_height - 35));
+        window.draw(FPS);
 
         grid.step();
         window.display();
